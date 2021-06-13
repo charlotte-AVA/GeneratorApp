91043800589eac389c90f7ce57bc70418d2f480c
charlotte-AVA <charlotte.mythen@avanade.com>
2021-06-13 20:46:06 +0100

    editing template files to be more project specific

---
+++ code/Merge/Controllers/MergeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Merge.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MergeController : ControllerBase
+    {
+        // applicationUrl: "http://localhost:3889" **SONGS**
+        // applicationUrl": "http://localhost:40971" **SINGERS**
+    }
+}
---
+++ code/Songs/Controllers/SongsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Songs.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SongsController : ControllerBase
+    {
+        private static readonly string[] Songs = new[]
+        {
+            "hello",
+            "song 2", 
+            "song 3",
+            "song 4",
+            "song 5",
+            "song 6",
+        }
+
+    }
+}
