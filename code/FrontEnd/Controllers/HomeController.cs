224d592c5f53b25ff1a9912297c41911acd299e0
charlotte-AVA <charlotte.mythen@avanade.com>
2021-06-15 14:59:59 +0100

    added gitignore and modified code but still FE error when deployed?

--- code/FrontEnd/Controllers/HomeController.cs
+++ code/FrontEnd/Controllers/HomeController.cs
@@ -7,15 +7,23 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using FrontEnd.Models;
 using System.Net.Http;
+using Microsoft.Extensions.Configuration; // added namespace (3)
 
 namespace FrontEnd.Controllers
 {
     public class HomeController : Controller
     {
+        private IConfiguration Configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // applicationUrl": "http://localhost:43813" **MERGE**
         public async Task<IActionResult> Index()
         {
-            var mergeService = "http://localhost:43813/merge";
+            //var mergeService = "http://localhost:43813/merge";
+            var mergeService = $"{Configuration["mergeServiceURL"]}/merge";
             var mergeResponseCall = await new HttpClient().GetStringAsync(mergeService);
             ViewBag.responseCall = mergeResponseCall;
             return View();
--- code/Merge/Controllers/MergeController.cs
+++ code/Merge/Controllers/MergeController.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http; // add namespace to enable service HttpClient for string asysnchronous values from char or int (not needed if alreay string?)
+using System.Net.Http; // // added namespace (1)
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration; // added namespace (2)
 using Microsoft.Extensions.Logging;
 
 namespace Merge.Controllers
@@ -14,13 +15,22 @@ namespace Merge.Controllers
     {
         // applicationUrl: "http://localhost:3889" **SONGS**
         // applicationUrl": "http://localhost:40971" **SINGERS**
+        private IConfiguration Configuration; 
+
+        public MergeController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         [HttpGet]
         public async Task<IActionResult> Get()
         {
-            var songsService = "http://localhost:3889/songs";
+            //var songsService = "http://localhost:3889/songs";
+            var songsService = $"{Configuration["songsServiceURL"]}/songs";
             var songsResponseCall = await new HttpClient().GetStringAsync(songsService);
 
-            var singersService = "http://localhost:40971/singers";
+            //var singersService = "http://localhost:40971/singers";
+            var singersService = $"{Configuration["singersServiceURL"]}/singers";
             var singersResponseCall = await new HttpClient().GetStringAsync(singersService);
 
             var mergeResponse = $"You will be singing {songsResponseCall} in the style of {singersResponseCall}";
