b70bf3d4015138ee3c97d4fe8d5697c2c34b247e
charlotte-AVA <charlotte.mythen@avanade.com>
2021-06-22 14:25:25 +0100

    Basic unit testing for service one and two controllers (also added TF output file with some notes for tomorrow)

---
+++ code/services.tests/serviceone/serviceOneTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Songs.Controllers;
+using Xunit;
+
+namespace services.tests.serviceone
+{
+    public class serviceOneTests //make this public!
+    {
+        [Fact] // make this a test method
+        public void GetTest()
+        {
+            SongsController songsController = new SongsController();
+            var songsControllerResult = songsController.Get();
+
+            Assert.NotNull(songsControllerResult);
+            Assert.IsType<ActionResult<string>>(songsControllerResult);
+        }
+    }
+}
---
+++ code/services.tests/servicetwo/serviceTwoTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Singers.Controllers;
+using Xunit;
+
+namespace services.tests.serviceone
+{
+    public class serviceTwoTests //make this public!
+    {
+        [Fact] // make this a test method
+        public void GetTest()
+        {
+            SingersController singersController = new SingersController();
+            var singersControllerResult = singersController.Get();
+
+            Assert.NotNull(singersControllerResult);
+            Assert.IsType<ActionResult<string>>(singersControllerResult);
+        }
+    }
+}
