f43ca0469d7f65d1272b48e82873b57989e79df1
charlotte-AVA <charlotte.mythen@avanade.com>
2021-06-22 15:09:44 +0100

    Mapped appsettings objects (service 1 and 2 URLS) from json file to new class file

---
+++ code/Merge/AppSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Merge
+{
+    public class AppSettings
+    {
+        public string songsServiceURL { get; set; }
+        public string singersServiceURL { get; set; }
+    }
+}
