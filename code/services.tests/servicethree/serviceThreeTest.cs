e68a4990d2c39108236f3489abbc53ee30b2a278
charlotte-AVA <charlotte.mythen@avanade.com>
2021-06-23 07:52:39 +0100

    setting up test for service 3 (merge) - first need to deploy

--- code/services.tests/servicethree/serviceThreeTest.cs
+++ code/services.tests/servicethree/serviceThreeTest.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace services.tests.servicethree
 {
-    class serviceThreeTest
+    public class serviceThreeTest
     {
+        //private AppSettings appSettings = new AppSettings()
+        //{
+        //    numbersServiceURL = "https://gwl-numbers.azurewebsites.net", //replace with SONGS URL
+        //    lettersServiceURL = "https://gwl-letters.azurewebsites.net" // replace with SINGERS URL
+        //};
+
+        //[Fact]
+        //public async void GetTest()
+        //{
+        //    var options = new Mock<IOptions<AppSettings>>();
+        //    options.Setup(x => x.Value).Returns(appSettings);
+
+        //    MergeController mergeController = new MergeController(options.Object);
+        //    var mergeContollerResult = await mergeController.Get();
+
+        //    Assert.NotNull(mergeContollerResult);
+        //    Assert.IsType<OkObjectResult>(mergeContollerResult);
+        //}
+
+
     }
 }
